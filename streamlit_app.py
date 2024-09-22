b0603459268bd4b2aef6686444495a6b7aedb11a
feranchesco <danial2000vafadar@gmail.com>
2024-09-22 22:26:17 +0330

    modifing streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ password = "1234"
 #فراخوانی دیتا بیس  و داده های جدول
 pro = Products("Data.db")
 all = pro.get_all_products()
+pro.close_query()
 st.set_page_config(layout="wide")
 
 with sidebar:
@@ -68,11 +69,12 @@ if selected == 'مدیریت':
         with editor:
             crud = option_menu(
                 menu_title=None,
-                options=['add','edit','delete'],
+                options=['کالای جدید','ویرایش کالا','حذف کالا'],
                 default_index=0,
                 orientation='horizontal',
             )
-            if crud == 'add':
+            if crud == 'کالای جدید':
+                pro = Products("Data.db")
                 #ساخت فرم برای اضافه کردن کالا
                 st.title("کالای جدید")
                 product = st.text_input("نام کالا")
@@ -86,7 +88,8 @@ if selected == 'مدیریت':
                     st.success("کالا با موفقیت اضافه شد")
                     #قطع ارتباط با دیتا بیس
                     pro.close_query()
-            if crud == 'edit':
+            if crud == 'ویرایش کالا':
+                pro = Products("Data.db")
                 st.title('تغییر در کالا')
                 product_id = st.text_input("شماره کالا")
                 product_name = st.text_input("نام کالا")
@@ -101,12 +104,15 @@ if selected == 'مدیریت':
                         inventory = st.number_input("موجودی در انبار", value=prodict_name[5])
                     except:
                         st.error('چنین محصولی نداریم')
+                        pro.close_query()
                     if st.button("تغییر"):
                         try:
                             pro.update_product(product_id,product, brand, buy_price, sell_price, inventory)
+                            pro.close_query()
                             st.success('با موفقیت تغییر پیدا کرد')
                         except:
                             st.error('نتونستم تغییرش بدم')
+                            pro.close_query()
 
                 if product_name :
                     prodict = pro.get_product_by_name(product_name)
@@ -117,22 +123,26 @@ if selected == 'مدیریت':
                     sell_price = st.text_input("قیمت فروش",value=prodict[4])
                     inventory = st.number_input("موجودی در انبار", value=prodict[5])
 
-            if crud == 'delete':
+            if crud == 'حذف کالا':
+                pro = Products("Data.db")
                 st.title("حذف کالا")
                 product_id = st.text_input("شماره کالا")
                 if product_id :
                     try:
                         prodict_name = pro.get_product_by_id(product_id)
-                        st.subheader('مشخصات کالای مورد نظر')
-                        st.write(prodict_name[1])
-                        st.write(prodict_name[2])
-                        st.write(prodict_name[3])
-                        st.write(prodict_name[4])
-                        st.write(prodict_name[5])
-                        if st.button('delete'):
+                        st.subheader(" مشخصات کالای مورد نظر")
+                        st.write(f"##### نام کالا:  {prodict_name[1]}")
+                        st.write(f"##### برند: {prodict_name[2]}")
+                        st.write(f"##### قیمت خرید:  {prodict_name[3]} ")
+                        st.write(f"##### قیمت فروش: {prodict_name[4]} ")
+                        st.write(f"##### موجودی در انبار : {prodict_name[5]} ")
+                        if st.button("##### حذف کالا"):
+                            # button click logic here
                             pro.delete_product(product_id)
+                            pro.close_query()
                     except:
                         st.error('چنین محصولی نداریم')
+                        pro.close_query()
 
 
 # df = pd.DataFrame(all, columns=[desc[0] for desc in pro.cursor.description])
